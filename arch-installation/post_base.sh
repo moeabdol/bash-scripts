60b7c094b59320b231e6dfa6daa87efefab1856d
Mohammed Saed <mohd.a.saed@gmail.com>
2022-06-01 23:41:49 +0300

    Modifying post_base installation script

--- arch-installation/post_base.sh
+++ arch-installation/post_base.sh
@@ -3,39 +3,15 @@
 echo "Starting post-base installation..."
 sleep 3
 
-mkdir -p /home/moeabdol/repos
-# yay
-cd /home/moeabdol/repos
-pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-yay -Y --gendb
-yay -Syu --devel
-yay -Y --devel --save
-cd /home/moeabdol
-
-# ly
-cd /home/moeabdol/repos
-git clone --recurse-submodules https://github.com/nullgemm/ly.git
-cd ly
-make
-sudo make install
-cd /home/moeabdol
-
 # datetime
+echo "Configure time/date"
+sleep 3
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-# yay packages
-yay -S \
-	lf-bin \
-	nerd-fonts-complete \
-	timeshift-autosnap \
-	timeshift-bin \
-	zramd
-
 # pacman packages
+echo "Installing pacman packages"
+sleep 3
 sudo pacman -S \
 	alacritty \
 	aria2 \
@@ -86,12 +62,62 @@ sudo pacman -S \
 	zathura-ps
 
 # pip packages
+echo "Installing python packages"
+sleep 3
 pip install --upgrade pip
 pip install \
 	ueberzug \
 	adblock
 
+echo "Creating repos directory"
+sleep 3
+mkdir -p /home/moeabdol/repos
+
+# yay
+echo "Installing yay packages"
+sleep 3
+cd /home/moeabdol/repos
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -Y --gendb
+yay -Syu --devel
+yay -Y --devel --save
+cd /home/moeabdol
+
+# ly
+echo "Installing ly display manager"
+sleep 3
+cd /home/moeabdol/repos
+git clone --recurse-submodules https://github.com/nullgemm/ly.git
+cd ly
+make
+sudo make install
+cd /home/moeabdol
+
+# fzf
+echo "Installing fzf"
+sleep 3
+cd /home/moeabdol/repos
+git clone --depth 1 https://github.com/junegunn/fzf.git
+cd fzf
+./install
+cd /home/moeabdol
+
+# yay packages
+echo "Installing yay packages"
+sleep 3
+yay -S \
+	lf-bin \
+	nerd-fonts-complete \
+	timeshift-autosnap \
+	timeshift-bin \
+	zramd
+
 # services
+echo "Enabling services"
+sleep 3
 sudo systemctl enable --now \
 	zramd \
 	ly
