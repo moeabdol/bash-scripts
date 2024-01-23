10919710c12f90af8021e05134b7a433c06e3f15
Mohammed Saed <mohd.a.saed@gmail.com>
2024-01-23 22:41:30 +0300

    Add chinese/japanese/korean fonts

--- arch-installation/post_base.sh
+++ arch-installation/post_base.sh
@@ -71,6 +71,16 @@ sudo pacman -S \
 	ttf-terminus-nerd \
 	xdotool \
 	noto-fonts-emoji
+	adobe-source-han-sans-cn-fonts \
+	adobe-source-han-sans-jp-fonts \
+	adobe-source-han-sans-kr-fonts \
+	adobe-source-han-sans-otc-fonts \
+	adobe-source-han-sans-tw-fonts \
+	adobe-source-han-serif-cn-fonts \
+	adobe-source-han-serif-jp-fonts \
+	adobe-source-han-serif-kr-fonts \
+	adobe-source-han-serif-otc-fonts \
+	adobe-source-han-serif-tw-fonts
 
 # pip packages
 echo "Installing python packages"
